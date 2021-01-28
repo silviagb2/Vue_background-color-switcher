9c306d36c5bea63c75daad8d0e02edfd7d2383c4
bcn-alumno <bcn-alumno@nm_madrid.local>
2021-01-28 14:30:22 +0100

    finished

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
-
-console.log(buttons);
-
-buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-        console.log(e.target);
-        if (e.target.id === 'grey'){
-            body.style.backgroundColor = e.target.id;
+const app = Vue.createApp({
+    methods:{
+        changeBackground(event){
+            document.body.style.background = event.target.id
         }
-        if (e.target.id === 'white'){
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id;
-        }
-        if (e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id;
-        }
-    })
+    }
 })
+
+app.mount('#app')
